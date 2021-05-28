5e881514ed17b462f254c8d0ef6e7c36c6af4343
TehBrian <tehbrian@outlook.com>
2021-05-28 14:55:41 -0400

    Initial commit

---
+++ src/main/java/xyz/tehbrian/project/Project.java
@@ -0,0 +1,17 @@
+package xyz.tehbrian.project;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Project extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
